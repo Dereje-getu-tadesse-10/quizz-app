047062fc68493144d0dcf73b5775a88461f4eb97
Dereje <getutadessedereje@icloud.com>
2022-06-17 20:41:22 +0200

    Update: get list questions

--- dist/play.js
+++ dist/play.js
@@ -1,1 +1,5 @@
-export {};
+import questions from './questions';
+const questionTitle = document.querySelector('.question-title');
+const answersButton = document.querySelectorAll('.option');
+questions.forEach((question, index) => {
+});
--- src/play.ts
+++ src/play.ts
@@ -1,1 +1,8 @@
 import questions from './questions';
+
+const questionTitle = document.querySelector('.question-title') as HTMLHeadingElement;
+const answersButton = document.querySelectorAll('.option') as NodeListOf<HTMLDivElement>; 
+
+questions.forEach((question, index) => {
+   
+});
