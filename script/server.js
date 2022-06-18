86341fae4e6063dd3c080760ab6f753b73f7e2ca
Dereje <getutadessedereje@icloud.com>
2022-06-18 14:19:12 +0200

    Update: connect to db with node js

--- script/server.js
+++ script/server.js
@@ -1,1 +1,22 @@
 const mysql = require('mysql');
+
+let db_host = 'localhost';; 
+let db_user_name = 'root';
+let db_password = 'root';
+
+const db = mysql.createConnection({
+    host: db_host,
+    user: db_user_name,
+    password: db_password,
+    database: 'quizz',
+    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+   
+})
+
+db.connect((err) => {
+    if (err) {
+        throw(err);
+    }
+    console.log('connected to database');
+})
+
